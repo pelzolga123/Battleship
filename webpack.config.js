6c841352dbac60ea203a50fd770043f6141f42c0
pelzolga123 <pelzolga123@gmail.com>
2019-12-04 13:13:06 +0200

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: / \.js$ /,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif)$/,
+        use: [
+          { loader: 'url-loader' },
+        ],
+      },
+    ],
+  },
+};
