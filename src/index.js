f9b2172a0b52605af2348badc2ef32187b0d0bed
pelzolga123 <pelzolga123@gmail.com>
2020-01-02 15:24:24 +0200

    Refractored functions, added turns

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 /* eslint-disable max-len */
 
 const SHIP = 0;
-const MISS = 1;
 const HIT = 2;
 let hitsMade = 0;
-// let hitsToWin;
+let hitsCount = 0;
 const ships = [5, 4, 3, 3, 2];
-// TODO: look into Int8Array on these big matrices for performance
 const positions = [];
 const probabilities = [];
-// const hitsSkewProbabilities = true;
-// const skewFactor = 2;
+
 const boardSize = 10;
 const classMapping = ['ship', 'miss', 'hit'];
 let board;
 let playerBoard;
 // let resultMsg;
 let volleyButton;
-// const monteCarlo = false;
+let turn = 'comp';
 
-// run immediately
 
 function recalculateProbabilities() {
   const hits = [];
@@ -37,12 +33,31 @@ function recalculateProbabilities() {
   }
 }
 
-// TODO onclick of cell with class name ship count the number of hits
-// TODO remove color of ships
-// TODO change color of( ship to red when cell is clicked to signify hits
 function fireAtWill() {
   const hit = 0;
 }
+
+const shootingMatrix = () => {
+  const matrix = new Set();
+
+  for (let i = 0; i <= 500; i += 1) {
+    const hitCoords = getRandomPosition();
+    const x = hitCoords[0];
+    const y = hitCoords[1];
+    const shootId = `${x}${y}`;
+    matrix.add(shootId);
+  }
+  return matrix;
+};
+const compMatrix = shootingMatrix();
+
+const matrixValues = () => {
+  const data = compMatrix.values();
+  const item = data.next().value;
+  compMatrix.delete(item);
+  console.log(compMatrix);
+  return item;
+};
 
 const getPlayerCells = (elemId) => {
   const table = document.getElementById('player-board');
@@ -73,21 +88,22 @@ const delayComputerMiss = (shootId) => {
   }, 2000);
 };
 
+const coords = () => {
+  const matrix = matrixValues();
+  return matrix;
+};
+
+
 const computerHit = () => {
-  let hit = 0;
-  while (hit <= 17) {
-    const hitCoords = getRandomPosition();
-    const x = hitCoords[0];
-    const y = hitCoords[1];
-    const shootId = `${x}${y}`;
-    const getCell = getPlayerCells(shootId);
-    if (getCell === 'ship') {
-      delayComputerShoot(shootId);
-      hit += 1;
-    } else {
-      delayComputerMiss(shootId);
-    }
+  const matrix = coords();
+  const getCell = getPlayerCells(matrix);
+  if (getCell === 'ship') {
+    delayComputerShoot(matrix);
+    hitsCount += 1;
+  } else {
+    delayComputerMiss(matrix);
   }
+  return hitsCount;
 };
 
 function setupBoard() {
@@ -98,7 +114,6 @@ function setupBoard() {
       positions[y][x] = null;
     }
   }
-
   distributeShips();
   recalculateProbabilities();
   redrawBoard(true);
@@ -123,7 +138,6 @@ function getRandomPosition() {
 }
 
 function randomBoolean() {
-  // return (Math.round(Math.random()) === 1);
   return (Math.random() >= 0.5);
 }
 
@@ -145,16 +159,31 @@ function placeShip(pos, shipSize, vertical) {
   return false;
 }
 
+
 function initialize() {
   board = document.getElementById('board');
   playerBoard = document.getElementById('player-board');
-  // resultMsg = document.getElementById('result');
-  // volleyButton = document.getElementById('volley');
-  // volleyButton.onclick = (monteCarlo ? runMonteCarlo : beginVolley);
   setupBoard();
   setupPlayerBoard();
 
-  computerHit();
+  while (computerHit() <= 16) {
+    console.log(turn);
+    console.log(computerHit());
+    if (turn === 'comp') {
+      computerHit();
+      turn = 'user';
+    } else if (turn === 'user') {
+      turn = 'comp';
+    }
+  }
+  /*
+  const winnerComp = computerHit();
+  if (winnerComp >= 16) {
+    const winnerDiv = document.getElementById('winner');
+    const winner = document.createElement('h1');
+    winner.innerHTML = 'You win!';
+    winnerDiv.appendChild(winner);
+  }*/
 }
 
 initialize();
@@ -172,9 +201,21 @@ function distributeShips() {
   }
 }
 
+const boord = document.getElementById('board');
+boord.addEventListener('click', (e) => {
+  for (let y = 0; y < boardSize; y += 1) {
+    for (let x = 0; x < boardSize; x += 1) {
+      if (e.target && e.target.id === 'ship') {
+        e.target.style.background = 'red';
+        hitsMade += 1;
+      } else {
+        e.target.style.background = 'blue';
+      }
+    }
+  }
+});
+
 function shipCanOccupyPosition(criteriaForRejection, pos, shipSize, vertical) {
-  // TODO: criteriaForRejection is an awkward concept, improve
-  // "pos" is ship origin
   const x = pos[0];
   const y = pos[1];
   const z = (vertical ? y : x);
@@ -194,7 +235,6 @@ function shipCanOccupyPosition(criteriaForRejection, pos, shipSize, vertical) {
 
 
 function redrawBoard(displayProbability) {
-  // if (monteCarlo) return; // no need to draw when testing thousands of boards
   let boardHTML = '';
   for (let y = 0; y < boardSize; y += 1) {
     boardHTML += '<tr>';
@@ -203,7 +243,6 @@ function redrawBoard(displayProbability) {
       boardHTML += '<td id="';
       if (thisPos !== null) boardHTML += classMapping[thisPos];
       boardHTML += '">';
-      // if (displayProbability && thisPos !== MISS && thisPos !== HIT) boardHTML += probabilities[x][y];
       boardHTML += '</td>';
     }
     boardHTML += '</tr>';
@@ -212,68 +251,8 @@ function redrawBoard(displayProbability) {
   playerBoard.innerHTML = boardHTML;
 }
 
-const boord = document.getElementById('board');
-boord.addEventListener('click', (e) => {
-  // e.preventDefault;
-  // e.target.style.background = 'red';
-  // fire(e);
-  // console.log(positions[1][2]);
-
-  for (let y = 0; y < boardSize; y += 1) {
-  // probabilities[y] = [];
-    for (let x = 0; x < boardSize; x += 1) {
-      // probabilities[y][x] = 0;
-      // we remember hits as we find them for skewing
-      // const name = document.getElementsByClassName('ship').value;
-      // console.log(positions[y][x]);
-
-      // console.log(name);
-      if (e.target && e.target.id === 'ship') {
-        e.target.style.background = 'red';
-        hitsMade += 1;
-      } else {
-        e.target.style.background = 'blue';
-      }
-
-      if (hitsMade === 17) {
-        /*
-        const winnerDiv = document.getElementById('winner');
-        const winner = document.createElement('h1');
-        winner.innerHTML = 'You win!';
-        winnerDiv.appendChild(winner);
-       */
-      }
-    }
-  }
-
-
-  // TODO if positions[x][y] == null
-  // TODO change background to blue
-  // TODO count it as a miss and not hit
-  // TODO if positions[x][y] == 0
-  // TODO change backgronud to red
-  // TODO count it as a hit and add 1 to the number of hits
-  // TODO check if number of hits is up to the one requred in the game
-});
-
 
 function fire(e) {
-//  const pos = getBestUnplayedPosition();
-  // const x = pos[0];
-  // const y = pos[1];
-
-  // if (positions[x][y] === SHIP) {
-  //   positions[x][y] = HIT;
-  //   // TODO change color of cell background
-  //   e.target.style.background = 'red';
-  //   hitsMade += 1;
-  // } else {
-  //   positions[x][y] = MISS;
-  //   e.target.style.background = 'blue';
-  // }
-
-  // recalculateProbabilities();
-  // redrawBoard(true);
   if (e.target && e.target.id === 'ship') {
     e.target.style.background = 'red';
     hitsMade += 1;
