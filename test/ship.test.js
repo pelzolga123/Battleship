8fedae682f6bce0da372885834c2bc4497b97c8b
tundeiness <tundeiness@yahoo.co.uk>
2020-01-07 11:21:44 +0100

    add few tests

---
+++ test/ship.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+jest.mock('../src/ship.js', () => ({
+  hits: jest.fn(),
+  isSunk: jest.fn(),
+}));
+const mockShip = require('../src/ship');
+
+describe('ships', () => {
+  it('Checks if hit has been called', () => {
+    mockShip.hits();
+    expect(mockShip.hits).toHaveBeenCalled();
+  });
+
+  it('Checks if isSunk has been called', () => {
+    mockShip.isSunk();
+    expect(mockShip.isSunk).toHaveBeenCalled();
+  });
+
+  it('Checks if Ships is an object', () => {
+    expect(typeof mockShip).toBe('object');
+  });
+});
