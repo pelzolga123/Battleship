0dd11c170c62cae858d022ecee2657fd3e1cb1c5
tundeiness <tundeiness@yahoo.co.uk>
2020-01-11 12:50:43 +0100

    run build

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,402 @@
-<<<<<<< HEAD
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";n.r(t);var r=e=>{const t=[];return{size:e,hitCoords:t,hit:e=>{t.push(e)},isSunk:()=>e===t.length}};let o=0;const l=0;let u=0,c="comp";const s=0,f=2,i=[],a=[],d=[],m=10,g=["ship","miss","hit"];let p,y;const b=[r(5),r(4),r(3),r(3),r(2)],h=[r(5),r(4),r(3),r(3),r(2)];console.log((()=>{let e=0;return h.forEach(t=>{!0===t.isSunk()&&(e+=1)}),5===e})());const v=(()=>{const e=new Set;for(let t=0;t<=500;t+=1){const t=B(),n=`${t[0]}${t[1]}`;e.add(n)}return e})(),M=e=>{const t=document.getElementById("player-board");for(let e=0;e<t.rows.length;e+=1)for(let n=0;n<t.rows[e].cells.length;n+=1){t.rows[e].cells[n].setAttribute("class",`${e}${n}`)}return document.getElementsByClassName(e)[0].id},E=e=>{document.getElementsByClassName(e)[0].style.background="red"},L=e=>{document.getElementsByClassName(e)[0].style.background="green"},S=()=>{return(()=>{const e=v.values().next().value;return v.delete(e),e})()};function j(){const e=S();return"ship"===M(e)?E(e):L(e),l}function k(){for(let e=0;e<m;e+=1){i[e]=[];for(let t=0;t<m;t+=1)i[e][t]=null}!function(){let e,t,n;for(let r=0,o=b.length;r<o;r+=1)for(t=!1,n=O();!t;)e=B(),t=w(e,b[r].size,n)}(),function(){const e=[];for(let t=0;t<m;t+=1){a[t]=[];for(let n=0;n<m;n+=1)a[t][n]=0,i[n][t]===f&&e.push([n,t])}}(),function(){let e="";for(let t=0;t<m;t+=1){e+="<tr>";for(let n=0;n<m;n+=1){const r=i[n][t];e+='<td id="',null!==r&&(e+=g[r]),e+='">',e+="</td>"}e+="</tr>"}p.innerHTML=e}()}function z(){for(let e=0;e<m;e+=1){i[e]=[];for(let t=0;t<m;t+=1)i[e][t]=null}!function(){let e,t,n;for(let r=0,o=h.length;r<o;r+=1)for(t=!1,n=O();!t;)e=B(),t=w(e,h[r].size,n)}(),function(){const e=[];for(let t=0;t<m;t+=1){d[t]=[];for(let n=0;n<m;n+=1)d[t][n]=0,i[n][t]===f&&e.push([n,t])}}(),function(){let e="";for(let t=0;t<m;t+=1){e+="<tr>";for(let n=0;n<m;n+=1){const r=i[n][t];e+='<td id="',null!==r&&(e+=g[r]),e+='">',e+="</td>"}e+="</tr>"}y.innerHTML=e}()}function B(){return[Math.floor(9*Math.random()),Math.floor(9*Math.random())]}function O(){return Math.random()>=.5}function w(e,t,n){const r=e[0],o=e[1],l=n?o:r,u=l+t-1;if(function(e,t,n,r){const o=t[0],l=t[1],u=r?l:o,c=u+n-1;if(c>m-1)return!1;for(let t=u;t<=c;t+=1){if((r?i[o][t]:i[t][l])===e)return!1}return!0}(s,e,t,n)){for(let e=l;e<=u;e+=1)n?i[r][e]=s:i[e][o]=s;return!0}return!1}const x=()=>{const e=document.getElementById("winner-h1"),t=("comp"===c?c="user":"user"===c&&(c="comp"),c);"comp"===t&&(y.classList.remove("freeze"),p.classList.add("freeze"),e.innerHTML="computer`s turn",j(),function(){const e=I();e?e():j()}(),console.log("comp",l)),"user"===t&&(y.classList.add("freeze"),p.classList.remove("freeze"),e.innerHTML=" user`s turn",I(),console.log("user"))};function I(){let e;return document.querySelectorAll("#board td").forEach(t=>t.addEventListener("click",t=>{t.target&&"ship"===t.target.id?(t.target.style.background="red",o+=1):t.target.style.background="blue",e=!0})),u+=1,e}!function(){p=document.getElementById("board"),y=document.getElementById("player-board"),k(),z();const e=setInterval(()=>{x(),16===l&&clearInterval(e)},5e3)}()}]);
-=======
-!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,t,r){"use strict";r.r(t);var n=e=>{const t=[];return{size:e,hitCoords:t,hit:e=>{t.push(e)},isSunk:()=>e===t.length}};var o=e=>{const t=["ship","miss","hit"],r=[],n=[],o=(e,t,n)=>{const o=e[0],l=e[1],s=n?l:o,u=s+t-1;if(((e,t,n,o)=>{const l=t[0],s=t[1],u=o?s:l,a=u+n-1;if(a>9)return!1;for(let t=u;t<=a;t+=1){if((o?r[l][t]:r[t][s])===e)return!1}return!0})(0,e,t,n)){for(let e=s;e<=u;e+=1)n?r[o][e]=0:r[e][l]=0;return!0}return!1};return{setupBoard:l=>{for(let e=0;e<10;e+=1){r[e]=[];for(let t=0;t<10;t+=1)r[e][t]=null}(()=>{let t,r,n;for(let l=0,s=e.length;l<s;l+=1)for(r=!1,n=Math.random()>=.5;!r;)t=[Math.floor(10*Math.random()),Math.floor(10*Math.random())],r=o(t,e[l].size,n)})(),(()=>{const e=[];for(let t=0;t<10;t+=1){n[t]=[];for(let o=0;o<10;o+=1)n[t][o]=0,2===r[o][t]&&e.push([o,t])}})(),(e=>{let n="";for(let e=0;e<10;e+=1){n+="<tr>";for(let o=0;o<10;o+=1){const l=r[o][e];n+='<td id="',null!==l&&(n+=t[l]),n+='">',n+="</td>"}n+="</tr>"}document.getElementById(e).innerHTML=n})(l)}}};let l=0,s="comp";const u=[n(5),n(4),n(3),n(3),n(2)],a=[n(5),n(4),n(3),n(3),n(2)];const c=(()=>{const e=new Set;for(let t=0;t<=500;t+=1){const t=[Math.floor(10*Math.random()),Math.floor(10*Math.random())],r=`${t[0]}${t[1]}`;e.add(r)}return e})(),d=e=>{const t=document.getElementById("playerBoard");for(let e=0;e<t.rows.length;e+=1)for(let r=0;r<t.rows[e].cells.length;r+=1){t.rows[e].cells[r].setAttribute("class",`${e}${r}`)}return document.getElementsByClassName(e)[0].id},i=e=>{document.getElementsByClassName(e)[0].style.background="red"},f=e=>{document.getElementsByClassName(e)[0].style.background="green"},m=()=>{return(()=>{const e=c.values().next().value;return c.delete(e),e})()};function p(){const e=m();return"ship"===d(e)?(i(e),l+=1):f(e),l}const g=()=>{const e=document.getElementById("winner-h1"),t=document.getElementById("board"),r=document.getElementById("playerBoard"),n=("comp"===s?s="user":"user"===s&&(s="comp"),s);"comp"===n&&(r.classList.remove("freeze"),t.classList.add("freeze"),e.innerHTML="computer`s turn",p(),console.log("comp",l)),"user"===n&&(r.classList.add("freeze"),t.classList.remove("freeze"),e.innerHTML=" user`s turn",function(){let e;document.querySelectorAll("#board td").forEach(t=>t.addEventListener("click",t=>{t.target&&"ship"===t.target.id?t.target.style.background="red":t.target.style.background="blue",e=!0}))}(),console.log("user"))};!function(){o(a).setupBoard("board"),o(u).setupBoard("playerBoard");const e=setInterval(()=>{g(),17===p()&&clearInterval(e)},500)}()}]);
->>>>>>> 6ed9c5516ba0a36ac214b22ff1c8d42625c61691
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./src/ships.js
+const Ship = (size) => {
+  const hitCoords = [];
+
+  const hit = (coords) => {
+    hitCoords.push(coords);
+    /* const hitSpot = document.getElementById(coords);
+    hitSpot.setAttribute('class', 'hit'); */
+  };
+
+  const isSunk = () => {
+    if (size === hitCoords.length) {
+      return true;
+    } return false;
+  };
+
+  return {
+    size,
+    hitCoords,
+    hit,
+    isSunk,
+  };
+};
+/* harmony default export */ var ships = (Ship);
+
+// CONCATENATED MODULE: ./src/board.js
+const Board = (userShips) => {
+  const boardSize = 10;
+  const classMapping = ['ship', 'miss', 'hit'];
+  const positions = [];
+  const HIT = 2;
+  const SHIP = 0;
+  const probabilities = [];
+
+  const getRandomPosition = () => {
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
+    return [x, y];
+  };
+
+  const randomBoolean = () => (Math.random() >= 0.5);
+
+  const shipCanOccupyPosition = (bool, pos, shipSize, vertical) => {
+    const x = pos[0];
+    const y = pos[1];
+    const z = (vertical ? y : x);
+    const end = z + shipSize - 1;
+
+    // board border is too close
+    if (end > boardSize - 1) return false;
+
+    // check if there's an obstacle
+    for (let i = z; i <= end; i += 1) {
+      const thisPos = (vertical ? positions[x][i] : positions[i][y]);
+      if (thisPos === bool) return false;
+    }
+
+    return true;
+  };
+
+  const placeShip = (pos, shipSize, vertical) => {
+    // "pos" is ship origin
+    const x = pos[0];
+    const y = pos[1];
+    const z = (vertical ? y : x);
+    const end = z + shipSize - 1;
+
+    if (shipCanOccupyPosition(SHIP, pos, shipSize, vertical)) {
+      for (let i = z; i <= end; i += 1) {
+        if (vertical) positions[x][i] = SHIP;
+        else positions[i][y] = SHIP;
+      }
+      return true;
+    }
+
+    return false;
+  };
+
+  const distributeShips = () => {
+    let pos; let shipPlaced; let vertical;
+    for (let i = 0, l = userShips.length; i < l; i += 1) {
+      shipPlaced = false;
+      vertical = randomBoolean();
+      while (!shipPlaced) {
+        pos = getRandomPosition();
+        shipPlaced = placeShip(pos, userShips[i].size, vertical);
+      }
+    }
+  };
+
+  const generate = () => {
+    const hits = [];
+    for (let y = 0; y < boardSize; y += 1) {
+      probabilities[y] = [];
+      for (let x = 0; x < boardSize; x += 1) {
+        probabilities[y][x] = 0;
+        if (positions[x][y] === HIT) {
+          hits.push([x, y]);
+        }
+      }
+    }
+  };
+
+  const redrawBoard = (boardName) => {
+    let boardHTML = '';
+    for (let y = 0; y < boardSize; y += 1) {
+      boardHTML += '<tr>';
+      for (let x = 0; x < boardSize; x += 1) {
+        const thisPos = positions[x][y];
+        boardHTML += '<td id="';
+        if (thisPos !== null) boardHTML += classMapping[thisPos];
+        boardHTML += '">';
+        boardHTML += '</td>';
+      }
+      boardHTML += '</tr>';
+    }
+    const board = document.getElementById(boardName);
+    board.innerHTML = boardHTML;
+  };
+
+  const setupBoard = (boardName) => {
+    // initialize positions matrix
+    for (let y = 0; y < boardSize; y += 1) {
+      positions[y] = [];
+      for (let x = 0; x < boardSize; x += 1) {
+        positions[y][x] = null;
+      }
+    }
+    distributeShips();
+    generate();
+    redrawBoard(boardName);
+  };
+
+  return {
+    setupBoard,
+  };
+};
+
+
+/* harmony default export */ var board = (Board);
+
+// CONCATENATED MODULE: ./src/index.js
+/* eslint-disable max-len */
+
+
+
+const hitsCount = 0;
+let turn = 'comp';
+
+// let volleyButton;
+
+const compShips = [
+  ships(5),
+  ships(4),
+  ships(3),
+  ships(3),
+  ships(2),
+];
+
+const userShips = [
+  ships(5),
+  ships(4),
+  ships(3),
+  ships(3),
+  ships(2),
+];
+
+const allShipsSunk = () => {
+  let count = 0;
+  compShips.forEach((ship) => {
+    if (ship.isSunk() === true) {
+      count += 1;
+    }
+  });
+
+  if (count === 5) {
+    return true;
+  } return false;
+};
+
+function getRandomPosition() {
+  const x = Math.floor(Math.random() * 10);
+  const y = Math.floor(Math.random() * 10);
+  return [x, y];
+}
+
+const shootingMatrix = () => {
+  const matrix = new Set();
+
+  for (let i = 0; i <= 500; i += 1) {
+    const hitCoords = getRandomPosition();
+    const x = hitCoords[0];
+    const y = hitCoords[1];
+    const shootId = `${x}${y}`;
+    matrix.add(shootId);
+  }
+  return matrix;
+};
+const compMatrix = shootingMatrix();
+
+const matrixValues = () => {
+  const data = compMatrix.values();
+  const item = data.next().value;
+  compMatrix.delete(item);
+  return item;
+};
+
+const getPlayerCells = (elemId) => {
+  const table = document.getElementById('playerBoard');
+  for (let i = 0; i < table.rows.length; i += 1) {
+    for (let j = 0; j < table.rows[i].cells.length; j += 1) {
+      const cell = table.rows[i].cells[j];
+      cell.setAttribute('class', `${i}${j}`);
+    }
+  }
+  const findElem = document.getElementsByClassName(elemId)[0].id;
+  return findElem;
+};
+
+
+const computerShoot = (shootId) => {
+  const data = document.getElementsByClassName(shootId)[0];
+  data.style.background = 'red';
+};
+
+const computerMiss = (shootId) => {
+  const data = document.getElementsByClassName(shootId)[0];
+  data.style.background = 'green';
+};
+
+const coords = () => {
+  const matrix = matrixValues();
+  return matrix;
+};
+
+
+function computerHit() {
+  const matrix = coords();
+  const getCell = getPlayerCells(matrix);
+  if (getCell === 'ship') {
+    computerShoot(matrix);
+    // hitsCount += 1;
+  } else {
+    computerMiss(matrix);
+  }
+  return hitsCount;
+}
+
+const getTurn = () => {
+  if (turn === 'comp') {
+    turn = 'user';
+  } else if (turn === 'user') {
+    turn = 'comp';
+  }
+  return turn;
+};
+
+const fight = () => {
+  const winner = document.getElementById('winner-h1');
+  const board = document.getElementById('board');
+  const playerBoard = document.getElementById('playerBoard');
+  const turns = getTurn();
+  if (turns === 'comp') {
+    playerBoard.classList.remove('freeze');
+    board.classList.add('freeze');
+    winner.innerHTML = 'computer`s turn';
+    computerHit();
+    console.log('comp', hitsCount);
+  }
+  if (turns === 'user') {
+    playerBoard.classList.add('freeze');
+    board.classList.remove('freeze');
+    winner.innerHTML = ' user`s turn';
+    addEvent();
+    console.log('user');
+  }
+};
+
+function initialize() {
+  board(userShips).setupBoard('board');
+  board(compShips).setupBoard('playerBoard');
+
+  const f = setInterval(() => {
+    fight();
+    if (computerHit() === 17) {
+      clearInterval(f);
+    }
+  }, 5000);
+}
+
+initialize();
+
+function addEvent() {
+  let tmp;
+
+  document.querySelectorAll('#board td').forEach((e) => e.addEventListener('click', (n) => {
+    if (n.target && n.target.id === 'ship') {
+      n.target.style.background = 'red';
+    } else {
+      n.target.style.background = 'blue';
+    }
+    tmp = true;
+  }));
+  return tmp;
+}
+
+
+/***/ })
+/******/ ]);
