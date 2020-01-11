015bef19d6610587a6a7f33ee74f90e5cf9119f2
tundeiness <tundeiness@yahoo.co.uk>
2020-01-11 15:00:20 +0100

    add babel to make testing easy

--- test/ships.test.js
+++ test/ships.test.js
@@ -1,32 +1,71 @@
-jest.mock('../src/ships.js', () => ({
-  createShip: jest.fn(),
-  hit: jest.fn(),
-  isSunk: jest.fn(),
-}));
+/* eslint-disable no-undef */
+import mockShip from '../src/ships';
+// import * as dependency from '../src/ships';
 
-jest.mock('../src/user.js', () => ({
-  user: jest.fn(),
-}));
 
-const mockShip = require('../src/ships');
+// const Ship = require('../src/ships');
+// const mockShip = {
+//   hit: jest.fn(),
+//   isSunk: jest.fn(),
+// };
 
-describe('ships', () => {
-  it('Checks if createShip has been called', () => {
-    mockShip.createShip();
-    expect(mockShip.createShip).toHaveBeenCalled();
-  });
+// jest.mock('../src/ships.js', () => mockShip);
 
-  it ('Checks if hit has been called', () => {
-    mockShip.hit();
-    expect(mockShip.hit).toHaveBeenCalled();
-  });
+// const mockShip = require('../src/ships');
 
-  it ('Checks if isSunk has been called', () => {
-    mockShip.isSunk();
-    expect(mockShip.isSunk).toHaveBeenCalled();
-  });
 
-  it ('Checks if Ships is an object', () => {
-    expect(typeof mockShip).toBe('object');
+describe('Ship', () => {
+  // it('Checks if hit has been called', () => {
+  //   mockShip.hit();
+  //   expect(mockShip.hit).toHaveBeenCalled();
+  // });
+
+  // it('Checks if Ships is an object', () => {
+  //   expect(typeof mockShip).toBe('object');
+  // });
+
+  it('isSunk() returns true if the ship hits are equal to the ship length', () => {
+    // dependency.hit = jest.fn();
+    // const ship = mockShip(4).isSunk();
+    // ship.hit();
+    // mockShip(4).isSunk();
+    const testShip = mockShip(4);
+    // testShip.hit(3);
+    expect(testShip.isSunk()).toBe(true);
+    // expect(mockShip(4).isSunk()).toBe(true);
   });
 });
+
+
+// jest.mock('../src/ships.js', () => ({
+//   createShip: jest.fn(),
+//   hit: jest.fn(),
+//   isSunk: jest.fn(),
+// }));
+
+// jest.mock('../src/user.js', () => ({
+//   user: jest.fn(),
+// }));
+
+// const mockShip = require('../src/ships');
+
+// describe('ships', () => {
+//   it('Checks if createShip has been called', () => {
+//     mockShip.createShip();
+//     expect(mockShip.createShip).toHaveBeenCalled();
+//   });
+
+//   it ('Checks if hit has been called', () => {
+//     mockShip.hit();
+//     expect(mockShip.hit).toHaveBeenCalled();
+//   });
+
+//   it ('Checks if isSunk has been called', () => {
+//     mockShip.isSunk();
+//     expect(mockShip.isSunk).toHaveBeenCalled();
+//   });
+
+//   it ('Checks if Ships is an object', () => {
+//     expect(typeof mockShip).toBe('object');
+//   });
+// });
